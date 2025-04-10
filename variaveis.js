bd2aa0b06f7d8f3fe64de943a5d14e7b2b8b4652
LeuBorelli <lej.borelli@gmail.com>
2025-04-09 23:04:01 -0300

    Módulo 3

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+/**
+ * Variáveis, let e var são basicamente a mesma coisa
+ */
+
+let nome = 'Leandro' // String delimitado por aspas simples
+var nomeDela = "Jessica" //String delimitada por aspas duplas
+let vidinha = `Bernardo` //Sting delimitada por crase
+let money = 12.000 // number
+let flatEarth = false // boolean
+var doidera = null // recebeu nulo
+var doidera2 =  // recebeu nada, até então está indefinida, é diferente de estar nulo
+
+console.log(doidera) // vai imprimir o que está na variável que esta entre parenteses
